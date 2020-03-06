48ee9530ba16d4ba12573d44ff8e5ca2624370a0
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-03-06 11:35:51 +0200

    flowerstore complete

--- src/FlowerStore/FlowersLoader.java
+++ src/FlowerStore/FlowersLoader.java
@@ -1,19 +1,39 @@
 package FlowerStore;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FlowersLoader {
 
-private FlowersLoader() {
+private FlowersLoader() {      //  private конструктор делает невозможным создание экземпляра этого класса
 }
 
-    public static List<FlowerStore.Flowers> load (String pathToFile) {
+    public static <T> List<T> load (String pathToFile) throws Exception {
 
-        List<FlowerStore.Flowers> flowersList = new ArrayList<>();
+        List<T> flowersList = new ArrayList<>();
 
-        System.out.println("Flowers Loader !");
-        System.out.println(pathToFile);
+        try {
+            FileReader reader = new FileReader(pathToFile);
+            Scanner scan = new Scanner(reader);
+            String line = scan.nextLine();
+
+            StringBuffer sb1 = new StringBuffer(line);
+            sb1.delete((line.length())-1, line.length());
+            line = String.valueOf(sb1);
+
+            StringBuffer sb2 = new StringBuffer(line);
+            sb2.delete(0, 1);
+            line = String.valueOf(sb2);
+
+            flowersList.add((T) line);
+
+            reader.close();
+        }
+        catch (Exception ex) {
+            System.out.println("Что-то с чтением из файла " + pathToFile + " пошло не так !");
+        }
 
         return flowersList;
     }
--- src/FlowerStore/FlowersSaver.java
+++ src/FlowerStore/FlowersSaver.java
@@ -1,16 +1,24 @@
 package FlowerStore;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class FlowersSaver {
 
-private FlowersSaver() {
+private FlowersSaver() {      //  private конструктор делает невозможным создание экземпляра этого класса
 }
 
-    public static void save(String pathToFile, List<FlowerStore.Flowers> flowersList) {
+    public static <T> void save(String pathToFile, List<T> flowersList) throws IOException {
 
-        System.out.println("Flowers Saver !");
-        System.out.println(flowersList);
+        try {
+             FileWriter writer = new FileWriter(pathToFile);
+             writer.write(String.valueOf(flowersList));
+             writer.close();
+        }
+        catch (Exception ex) {
+             System.out.println("Что-то с записью в файл " + pathToFile + " пошло не так !");
+        }
 
     }
 
